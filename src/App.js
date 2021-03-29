adbda9f2de26fe493bab007c3fc8d0ea60eb7a27
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-03-29 14:22:01 -0300

    Static structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import data from './people.json';
+import People from './components/People';
 
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      people: data.people,
+      currentDisplay: data.people.filter(person => person.id === 3)
+    }
+
+    this.display = this.display.bind(this);
+  }
+
+  display(id) {
+    this.setState({
+      currentDisplay: data.people.filter(person => person.id === id)
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <div className="title">
+          <h1>Experience</h1>
+        </div>
+        <People people={this.state.people} displayButton={this.display} current={this.state.currentDisplay} />
       </div>
     );
   }
--- src/components/People.js
+++ src/components/People.js
@@ -1,10 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+
+function filterDuration(duration) {
+    let startDate = new Date(duration[0]+"-1");
+    let startYear = startDate.getFullYear();
+    let startMonth = startDate.toLocaleString('default', { month: 'short' });
+    if(duration.length === 1) {
+        return startMonth + " " + startYear + " - Present";
+    } else {
+        let endDate = new Date(duration[1]+"-1");
+        let endYear = endDate.getFullYear();
+        let endMonth = endDate.toLocaleString('default', { month: 'short' });
+        return startMonth + " " + startYear + " - " + endMonth + " " + endYear;
+    }
+}
 
 export default class People extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="container">
+                <div className="tabs">
+                    {this.props.people.map(person => (
+                        <button onClick={() => this.props.displayButton(person.id)} className="tab-button">{person.name.toUpperCase()}</button>
+                    ))}
+                </div>
+
+                <div className="display">
+                    {this.props.current.map(current => (
+                        <div className="stats">
+                            <p className="role">{current.role}</p>
+                            <p className="name">{current.name}</p>
+                            <p className="duration">{filterDuration(current.duration)}</p>
+                            <ul>
+                                {current.bulletPoints.map(point => (
+                                    <li>{point}</li>
+                                ))}
+                            </ul>
+                            <button className="more-info-btn">More Info</button>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
